b5764761418d2454cb7e2a9e2262c728d0914a19
beastman3226 <beastman3226@hotmail.com>
2013-10-24 15:06:26 -0400

    Pulling

--- src/me/beastman3226/iq/data/handler/Query.java
+++
@@ -1,9 +0,0 @@
-package me.beastman3226.iq.data.handler;
-
-/**
- *
- * @author beastman3226
- */
-public class Query extends DataHandler {
-
-}
--- src/me/beastman3226/iq/errors/DatabaseNotSupportedException.java
+++
@@ -1,8 +0,0 @@
-package me.beastman3226.iq.errors;
-
-/**
- *
- * @author beastman3226
- */
-public class DatabaseNotSupportedException extends Exception {
-}
